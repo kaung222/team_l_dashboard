5e320abb18c80d513730adba18ca8a3faf053114
Lenovo <aunkyawin2035@gmail.com>
2023-06-24 01:35:05 +0630

    sidebarHeaderUpdate

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,10 @@ import Sidebar from "./Sidebar";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
+
   return (
     <>
-      <div className="bg-slate-200 min-h-screen fixed top-0 left-0 w-[300px]">
+      <div className="bg-[#0C768A] min-h-screen fixed top-0 left-0 w-[310px]">
         <Sidebar />
       </div>
       <div className=" ml-[300px] h-16 bg-blue-400">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,102 @@
-const Sidebar = () => {
-  return <div className=" w-full h-full"></div>;
-};
+// import React from 'react'
+import {RiMenu2Line} from 'react-icons/ri'
+import {BsChevronDown} from 'react-icons/bs'
+import { useState } from 'react'
 
-export default Sidebar;
+const Sidebar = () => {
+  const [user,setUser]  = useState({
+    id : 4,
+    name : 'Reporting',
+    firstName : 'E',
+    team : "Team Reporting"
+  });
+  console.log(user)
+  const userData = [
+    {
+      id : 1,
+      name : 'CRM',
+      firstName : 'C',
+      team : "Designer Team"
+    },
+    {
+      id : 2,
+      name : 'Application Design',
+      firstName : 'A',
+      team : "Flutter Devs"
+    },
+    {
+      id : 3,
+      name : 'Ecomerce',
+      firstName : 'E',
+      team : "Developer Team"
+    },
+    {
+      id : 4,
+      name : 'Reporting',
+      firstName : 'E',
+      team : "Team Reporting"
+    }
+  ]
+  return (
+    <div>
+      {/* Sidebar */}
+      <div className="">
+        {/* Sidebar Brand */}
+        <div className=" justify-between items-center px-5 py-8 hidden xl:flex">
+          <a href="" className=''>
+            <span className=''>
+              <img className=' w-[104px]' src="https://themesdesign.in/tocly/layouts/assets/images/logo-light.png" alt="" />
+            </span>
+          </a>
+          <button className=" text-white text-3xl font-bold">
+           <RiMenu2Line />
+          </button>
+        </div>
+    {/* User Dropdown */}
+      <div className="">
+        <button className="dropdown px-5 py-1 w-full ">
+          <summary className=" bg-[#308A9B]">
+            <div className=" flex justify-between items-center  text-white p-3">
+              <div className=" flex">
+                <div className="border rounded-full bg-[#F2F6F7] h-10 flex items-center mt-2">
+                    <div className=' p-4 text-xl font-bold text-[#0C768A]'>{user?.firstName}</div>
+                </div>
+                <div className=" ml-2 text-left justify-center">
+                  <div className=' font-bold text-lg'>{user?.name}</div>
+                  <div className=' text-[#C1DCE1]'>{user?.team}</div>
+                </div>
+              </div>
+              <div className="">
+                <BsChevronDown />
+              </div>
+            </div>
+          </summary>
+          <ul className=" dropdown-content z-[1] rounded bg-white w-[270px]">
+          {userData?.map(user => (       
+            <li onClick={() => setUser(user)} key={user?.id}>
+              <div className="flex justify-between items-center text-white p-3">
+                <div className="flex">
+                  <div className="border rounded-full bg-[#0C768A] h-10 flex items-center mt-2">
+                    <div className="p-4 text-xl font-bold text-[#F2F6F7]">{user?.firstName}</div>
+                  </div>
+                  <div className="ml-2 text-left justify-center">
+                    <div className="font-bold text-lg text-[#0C768A]">{user?.name}</div>
+                    <div className="text-[#A8A8AF]">{user?.team}</div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+          </ul>
+        </button>
+       </div>
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
+
