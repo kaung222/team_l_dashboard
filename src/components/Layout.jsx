a5619d54c20064cd1484200fd2c326a7b55a01d7
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2023-06-24 20:49:27 +0630

    Status Card Complete 1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
       <div className="bg-slate-200 min-h-screen fixed top-0 left-0 w-[300px]">
         <Sidebar />
       </div>
-      <div className=" ml-[300px] h-16 bg-blue-400">
+      <div className=" md:ml-[300px] h-16 bg-blue-400">
         <Navbar />
         {children}
       </div>
